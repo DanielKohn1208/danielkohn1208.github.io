c666f8bccefda7aeec58b5da545cf9bceb02d322
DanielKohn1208 <danielmkohn1208@gmail.com>
2024-02-11 16:24:26 -0500

    added google analytics but it is going to work well this time

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,9 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
-import { VitePluginRadar } from 'vite-plugin-radar'
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
-		VitePluginRadar({
-			analytics:{
-				id: 'G-07D766DS4L',
-			},
-			enableDev:  false,
-		}),
 		svelte()
 	],
 })
