8d9b28c25ab1fad1fd3d4f6ebb5af9287f99b691
DanielKohn1208 <danielmkohn1208@gmail.com>
2024-02-11 16:17:14 -0500

    added google analytics

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { VitePluginRadar } from 'vite-plugin-radar'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+	plugins: [
+		VitePluginRadar({
+			analytics:{
+				id: 'G-07D766DS4L',
+			},
+			enableDev:  false,
+		}),
+		svelte()
+	],
 })
